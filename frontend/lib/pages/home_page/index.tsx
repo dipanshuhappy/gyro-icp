a3220cb8ce6aeddba609754cf62cb3fbcc780dc6
Nithin-Varma <mnithin1422@gmail.com>
2023-04-08 18:18:30 +0530

    new home page

---
+++ frontend/lib/pages/home_page/index.tsx
@@ -0,0 +1,13 @@
+import { Grid } from "@chakra-ui/react";
+import HomeHeader from "../../layout/HomeHeader";
+import React from "react"
+const Home = () => {
+    return (
+    <Grid>
+        <HomeHeader />
+
+    </Grid>
+    );
+};
+
+export default Home;
