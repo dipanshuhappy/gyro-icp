fc24646a9d81c487ff04f3dc73d1bd679656c7a8
Nithin-Varma <mnithin1422@gmail.com>
2023-04-10 00:19:01 +0530

    some changes in home

--- frontend/lib/pages/home_page/index.tsx
+++ frontend/lib/pages/home_page/index.tsx
@@ -1,14 +1,16 @@
-import { Grid } from "@chakra-ui/react";
+import { Grid, Box } from "@chakra-ui/react";
 import HomeHeader from "../../layout/HomeHeader";
 import Route from "./components/route";
 import React from "react"
 const Home = () => {
     return (
+    // <Box w="100%" h="100%" bgGradient="linear(to-l,  gray.700, #4F0079)">
     <Grid>
         <HomeHeader />
         <Route />
 
     </Grid>
+    // </Box>
     );
 };
 
